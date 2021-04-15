4ec68c7dcb17c40f07a572785526c6327f996ba5
Fabian Lemke <info@kikkirej.net>
2021-04-15 19:57:00 +0200

    :tada: start.spring.io generated content

---
+++ src/main/java/net/kikkirej/stats/server/StatsCollectorServerApplication.java
@@ -0,0 +1,13 @@
+package net.kikkirej.stats.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsCollectorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsCollectorServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kikkirej/stats/server/StatsCollectorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kikkirej.stats.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsCollectorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
